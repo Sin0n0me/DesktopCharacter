3ae8c2ea4ea49e1210e86faf9c083c820f70adcc
Sin0n0me <zakokamikami@icloud.com>
2026-02-12 15:02:50 +0900

    ビルド用cmdの更新

--- build.cmd
+++ build.cmd
@@ -1,8 +1,12 @@
 @echo off
 
+set BUILD_DIR_NAME=build_cmake
+
+@rem bullet
+cd %~dp0
 cd lib\bullet
-mkdir build_cmake
-cd build_cmake
+mkdir %BUILD_DIR_NAME%
+cd %BUILD_DIR_NAME%
 cmake ^
     -DBUILD_UNIT_TESTS=OFF ^
     -DBUILD_BULLET2_DEMOS=OFF ^
@@ -21,3 +25,24 @@ cmake ^
 
 cmake --build . --config Release
 cmake --build . --config Debug
+
+
+@rem glm
+cd %~dp0
+cd lib\glm
+mkdir %BUILD_DIR_NAME%
+cd %BUILD_DIR_NAME%
+cmake ^
+    -DGLM_BUILD_TESTS=OFF ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -G "Visual Studio 17 2022" ..
+    
+cmake --build . --config Release
+cmake --build . --config Debug
+
+
+@rem imgui
+cd %~dp0
+cd lib\imgui
+mkdir %BUILD_DIR_NAME%
+cd %BUILD_DIR_NAME%
